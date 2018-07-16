a12e459b8b3e54c3b2dd40de8fd56edb4b181f53
AmmarMasood <ammar.masood98@gmail.com>
2018-07-16 13:35:07 +0500

    fixxed some errors

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ app.use(express.static('client/build'));
 
 const path = require('path');
 app.get('*', (req,res) => {
-	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-})
+	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 }
 
 const PORT = process.env.PORT || 5000;
