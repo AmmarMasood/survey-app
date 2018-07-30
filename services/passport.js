169b94bf00980e028cd9652948c2743afa499648
AmmarMasood <ammar.masood98@gmail.com>
2018-07-30 17:29:04 +0500

    fixing heroku issues

--- services/passport.js
+++ services/passport.js
@@ -25,9 +25,8 @@ passport.use(
    const existingUser = await User.findOne({googleId: profile.id})
     if (existingUser) {
       done(null, existingUser);
-    } else {
+    }
      const user = await new User({googleId: profile.id}).save()
      done(null, user);
-    }
   })
 );
